13532419023b2fc7fa1c2913c8fda56b07e2dd43
gsimp0 <134464717+gsimp0@users.noreply.github.com>
2026-03-04 14:24:28 +1000

    Update letters.js

--- js/letters.js
+++ js/letters.js
@@ -1,37 +1,26 @@
 const impressions = [
   {
     title: "Belltop",
-    body: `You felt calm.
-I felt like you were someone I could trust quickly.
+    body: `When I met you at belltop for the first time, I felt like you were someone I could trust quickly.
 I remember thinking how cool you were. How much I wanted to get to know you more. I was so excited to find out the things we had in common. I remember thinking how I rarely if ever meet someone that gets excited about the same things I do in the way you did.`
   },
   {
     title: "Watermelon",
-    body: `I remember sitting eating my watermon at the bottom of the psych building. In teh distance I caught a glace of a group walking up the hill. I could see you!
-I was so happy to see you and even more happy that when you got to the stairs to turned and said goodbye to the group and walked over to me.
-I was trying to be so cool.`
+    body: `I remember sitting eating my watermon at the bottom of the psych building. In the distance I caught a glance of a group walking up the hill. I could see you!
+I was so happy to see you and even more happy that when you got to the stairs to turned and said goodbye to the group and walked over to me.`
   },
   {
-    title: "How you looked at the world",
-    body: `You noticed things.
-In the most a caring way. I saw that you knew everything had something special about it and if anyone one was going to celebrate them it would be you..`
+    title: "IHow you looked at the world",
+    body: `I loved first noticing the way you saw things.
+In the most a caring way. I saw that you knew everything had something special about it and if anyone one was going to celebrate them it would be you.`
   },
   {
-    title: "Your laugh",
-    body: `It made the room feel less serious.
-Like it gave everything permission to soften.`
-  },
-  {
-    title: "The way you moved",
-    body: `You seemed self-possessed.
-Not performing.
-Just fully there.`
+    title: "An earth person",
+    body: `Like your mum says. But I also like to think you're an earth person because you are one of those people that are good at life. I remember sitting in the van with you and you were telling me about building the tiny house. You soak up every second of life. My goodness you've built/taken-apart an engine. The task of listing your skills and experiences is too large to quantify in this sentence.`
   },
   {
     title: "The feeling after",
-    body: `I left thinking about you.
-Not in a dramatic way.
-In a quiet, certain way.`
+    body: `Everything felt normal. Everything felt right. It didn't feel scary. I can't imagine a universe where I didn't at least confess to you at least this.`
   }
 ];
 
